a0b08fe16682e89eb9ee580c4e6aba51646cf604
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-25 14:28:56 +0800

    feat(GambleCommand): pair games

--- src/plugins/command/modules/Currency/rich.ts
+++ src/plugins/command/modules/Currency/rich.ts
@@ -1,6 +1,19 @@
 import { Snowflake, GuildMember, Collection, Message } from 'discord.js';
 import { Command, Context, CurrencyEntry, Colors } from 'lava/index';
 
+const emojis = [
+	'<:prestigeI:733606604326436897>',
+	'<:prestigeII:733606705287397407>',
+	'<:prestigeIII:733606758727024702>',
+	'<:prestigeIV:733606800665870356>',
+	'<:prestigeV:733606838523920405>',
+	'<:prestigeVI:733606963471974500>',
+	'<:prestigeVII:733607038969577473>',
+	'<:prestigeVIII:733608252562079784>',
+	'<:prestigeIX:733607250584797214>',
+	'<:prestigeX:733607342263894056>',
+];
+
 export default class extends Command {
 	constructor() {
 		super('rich', {
@@ -26,10 +39,11 @@ export default class extends Command {
 			footer: { text: 'These are WALLETS, not net worths' },
 			color: Colors.RED, description: await this.top(ctx.guild.members.cache)
 				.then(docs => docs.map((doc, i) => {
-					// @TODO: prestige icons
 					const user = ctx.client.users.cache.get(doc.cache._id)?.tag ?? 'LOL WHO DIS';
 					const emoji = Array(3).fill('coin')[i] ?? 'small_red_triangle';
-					return `**:${emoji}: ${doc.props.pocket.toLocaleString()}** - ${user}`;
+					const prestige = doc.props.prestige.level;
+					const prestigeIcon = emojis[prestige - 1] ?? emojis[emojis.length - 1];
+					return `**:${emoji}: ${doc.props.pocket.toLocaleString()}** - ${prestige >= 1 ? prestigeIcon : ''}${user}`;
 				}).join('\n')),
 		}]}).then(() => false);
 	}
