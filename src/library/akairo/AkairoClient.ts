25d174cf24778a65ed2115d0b1adc5fd4b038ea6
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-29 10:12:57 +0800

    smh

--- src/library/akairo/AkairoClient.ts
+++ src/library/akairo/AkairoClient.ts
@@ -4,7 +4,7 @@
 */
 
 import { ClientUtil, SpawnHandler, DonationHandler, ArgumentHandler, QuestHandler, InhibitorHandler, SettingHandler, CommandHandler, ListenerHandler, ItemHandler, PluginManager } from '.';
-import { ClientOptions, MessageOptions, TextChannel } from 'discord.js';
+import { ClientOptions, MessageOptions, TextChannel, UserResolvable, Message, User, GuildMember, ThreadMember } from 'discord.js';
 import { AkairoClient, AkairoOptions } from 'discord-akairo';
 import { Connector, Logger, Imgen } from 'lava/index';
 import { join } from 'path';
@@ -46,6 +46,20 @@ export class LavaClient extends AkairoClient {
 	public plugins = new PluginManager(this, {
 		directory: join(__dirname, '..', '..', 'plugins')
 	}).loadAll();
+
+	/**
+	 * Check whether someone is a bot owner.
+	 */
+	public isOwner(res: UserResolvable) {
+		const owners = Array.isArray(this.ownerID) ? [...this.ownerID] : [this.ownerID];
+		return owners.some(o => {
+			if (res instanceof Message) return o === res.author.id;
+			if (res instanceof User) return o === res.id;
+			if (res instanceof GuildMember) return o === res.user.id;
+			if (res instanceof ThreadMember) return o === res.user.id;
+			return o === res;
+		});
+	}
 
 	/**
 	 * Shortcut to our handlers from our plugins.
