42eb4fb1bec21086fb5d0740fa90b93fb5f45a34
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-08-03 21:02:22 +0800

    refactor(UtilityCommand): buttons and buttons everywhere

--- src/plugins/command/modules/Utility/rcr.ts
+++ src/plugins/command/modules/Utility/rcr.ts
@@ -1,5 +1,5 @@
-import { Command} from 'lava/index';
-import { Message } from 'discord.js';
+import { Message, MessageButton, MessageActionRow, ButtonInteraction } from 'discord.js';
+import { Command } from 'lava/index';
 
 export default class extends Command {
 	constructor() {
@@ -13,26 +13,55 @@ export default class extends Command {
 	}
 
 	async exec(ctx: Message) {
-		const rcr = await ctx.guild.roles.fetch('716344676634066964');
-		const color = ctx.client.util.randomColor();
+		const rcr = ctx.guild.roles.resolve('716344676634066964') ?? await ctx.guild.roles.fetch('716344676634066964', { force: true });
+		const firstColor = ctx.client.util.randomColor();
 
-		const prompt = async (color: number): Promise<number | Function> => {
-			await ctx.reply({ embeds: [{ color, description: 'Do you like this color? Type `(y / n)` only.' }]});
-			const choice = await ctx.awaitMessage();
-			switch(choice.content.toLowerCase().slice(0, 1)) {
-				case 'n':
+		const btns = ['Yes', 'No'].map(label => new MessageButton({
+			label, customId: label.toLowerCase(), style: 'SECONDARY'
+		}));
+		const components = [new MessageActionRow({ components: btns })];
+		const msg = await ctx.reply({ 
+			components, embeds: [{
+				title: 'Do you like this color?',
+				color: firstColor
+			}]
+		});
+
+		const disabledStates = [new MessageActionRow({
+			components: [...msg.components.flatMap(row => 
+				row.components.filter(c => c.type === 'BUTTON')
+			).map(btn => btn.setDisabled(true))]
+		})];
+
+		const prompt = async (color: number): Promise<boolean | number | Function> => {
+			const choice = await msg.awaitMessageComponent<ButtonInteraction>({
+				time: 10000, filter: int => int.user.id === ctx.author.id
+			}).catch(() => {});
+
+			if (!choice) {
+				await msg.edit({ 
+					components: disabledStates, 
+					embeds: msg.embeds.map(e => ({
+						title: 'You should answer the question next time.',
+						color: null
+					})) 
+				});
+				return false;
+			}
+
+			switch(choice.customId) {
+				case 'no':
+					await msg.edit({ components, embeds: msg.embeds.map(e => ({ ...e, color })) });
 					return prompt(ctx.client.util.randomColor());
-				case 'y':
-				default:
+				case 'yes':
 					return color;
 			}
 		};
 
-		const newColor = await prompt(color) as number;
-		await rcr.edit({ color: newColor });
-		return ctx.reply({ embeds: [{
-			description: `Ok, color changed. Enjoy!`,
-			color: newColor,
-		}]}).then(() => true);
+		const color = await prompt(firstColor);
+		if (!color) return false;
+		await rcr.edit({ color: color as number }).catch(() => {});
+		await msg.edit({ components: disabledStates, embeds: [{ title: 'Color changed, enjoy!' }] });
+		return true;
 	}
 }
