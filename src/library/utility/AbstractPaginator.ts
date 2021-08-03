3b338e4c9be313c993862ef3479131b540be221e
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-08-03 11:00:13 +0800

    fix(Paginator): limits and misc changes

--- src/library/utility/AbstractPaginator.ts
+++ src/library/utility/AbstractPaginator.ts
@@ -1,6 +1,8 @@
 import { Message, Collection, MessageOptions, MessageActionRow, InteractionCollector, ButtonInteraction } from 'discord.js';
 import { LavaClient } from 'lava/akairo';
 
+type PaginatorPage = Omit<MessageOptions, 'components'>;
+
 /**
  * Controls:
  * 'first' - first page
@@ -48,7 +50,7 @@ interface PaginatorOptions {
 	/**
 	 * The pages to paginate.
 	 */
-	pages: MessageOptions[];
+	pages: PaginatorPage[];
 }
 
 class AbstractPaginator {
@@ -57,13 +59,20 @@ class AbstractPaginator {
 	public message: Message;
 	public current: number;
 	public timeout: number;
-	public pages: MessageOptions[];
+	public pages: PaginatorPage[];
 
 	/**
 	 * @param message the message sent to the channel/user
 	 * @param options the options for this paginator
 	 */
 	public constructor(options: PaginatorOptions) {
+		if (options.pages.length < 1) {
+			throw new TypeError('Property "pages" is empty');
+		}
+		if (options.focus > options.pages.length - 1) {
+			throw new TypeError('Property "focus" is greater than pages length');
+		}
+
 		this.controls = options.controls;
 		this.current = options.focus ?? 0;
 		this.message = options.message;
@@ -71,19 +80,29 @@ class AbstractPaginator {
 		this.pages = options.pages;
 
 		this.collector = this.message.createMessageComponentCollector<ButtonInteraction>({
-			filter: interaction => interaction.user.id === this.message.author.id,
 			time: this.timeout,
+			filter: interaction => {
+				const controlIds = Object.values(PaginatorControlId);
+				return controlIds.some(id => id === interaction.customId) 
+					&& interaction.user.id === this.message.author.id;
+			},
 		});
 
-		this.collector.on('collect', this.handleIncoming.bind(this));
-		this.collector.on('end', this.handleEnd.bind(this));
+		this.collector.on('collect', this._handleIncoming.bind(this));
+		this.collector.on('end', this._handleEnd.bind(this));
 	}
 
-	public async handleEnd(collected: Collection<Snowflake, ButtonInteraction>, reason: string) {
+	private async _disableAll() {
+		const buttons = this.message.components.flatMap(row => row.components.filter(c => c.type === 'BUTTON')).map(c => c.setDisabled(true));
+		await this.message.edit({ ...this.pages[this.current], components: [new MessageActionRow({ components: [...buttons] })] });
+	}
+
+	public async _handleEnd(collected: Collection<Snowflake, ButtonInteraction>, reason: string) {
+		await this.message.edit(this.pages[0]);
 		await this._disableAll();
 	}
 
-	public async handleIncoming(int: ButtonInteraction) {
+	public async _handleIncoming(int: ButtonInteraction) {
 		switch(int.customId) {
 			case PaginatorControlId.FIRST:
 				await this.message.edit(this.pages[this.current = 0]);
@@ -108,11 +127,6 @@ class AbstractPaginator {
 			default:
 				throw new TypeError('Invalid Button ID');
 		}
-	}
-
-	private async _disableAll() {
-		const buttons = this.message.components.flatMap(row => row.components.filter(c => c.type === 'BUTTON')).map(c => c.setDisabled(true));
-		await this.message.edit({ ...this.pages[this.current], components: [new MessageActionRow().addComponents(...buttons)] });
 	}
 }
 
