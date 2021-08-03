20d76a339b9f5ec507b0d3cfb9355e395f867f86
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-08-03 13:20:22 +0800

    fix: buttons

--- enum.js
+++
@@ -1,9 +0,0 @@
-var PaginatorControlId;
-(function (PaginatorControlId) {
-    PaginatorControlId["FIRST"] = "first";
-    PaginatorControlId["PREVIOUS"] = "prev";
-    PaginatorControlId["STOP"] = "stop";
-    PaginatorControlId["NEXT"] = "next";
-    PaginatorControlId["LAST"] = "last";
-})(PaginatorControlId || (PaginatorControlId = {}));
-console.log(Object.values(PaginatorControlId));
--- enum.ts
+++
@@ -1,9 +0,0 @@
-enum PaginatorControlId {
-	FIRST = 'first',
-	PREVIOUS = 'prev',
-	STOP = 'stop',
-	NEXT = 'next',
-	LAST = 'last'
-}
-
-console.log(Object.values(PaginatorControlId));
